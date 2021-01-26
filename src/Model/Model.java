373890eefc5d07d51261a33baae9afee3eb0b293
Rafael Ehammer <Rafael.ehammer@htl-wels.at>
2021-01-26 13:25:55 +0100

    implemented changeColorViaAbsoluteValue

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -2,9 +2,33 @@ package Model;
 
 public class Model
 {
-    private ModularCounter red = new ModularCounter(0,256);
-    private ModularCounter green = new ModularCounter(0,256);
-    private ModularCounter blue = new ModularCounter(0,256);
+    private static ModularCounter red = new ModularCounter(0,256);
+    private static ModularCounter green = new ModularCounter(0,256);
+    private static ModularCounter blue = new ModularCounter(0,256);
 
+    public static void changeColorViaAbsoluteValue (ColorCode colorcode, int value)
+    {
+        switch (colorcode) {
+            case RED:
+                red.reset();
+                red.inc(value);
+                break;
+            case BLUE:
+                blue.reset();
+                blue.inc(value);
+                break;
+            case GREEN:
+                green.reset();
+                green.inc(value);
+                break;
+        }
+    }
+    public static void changeColorViaRelativeValue (ColorCode colorcode, String value)
+    {
+        
+    }
+    public static String getHex()
+    {
 
+    }
 }
